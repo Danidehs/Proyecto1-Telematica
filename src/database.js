50a5eb5fd0dc70aa32bd4411560f0ae1db903d25
danidehs <hsdaniele95@gmail.com>
2019-08-13 20:24:58 -0500

    Fixing errors

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const URI = 'mongodb://localhost/proyecto1-telematica'
+const URI = 'mongodb://mongo-server/proyecto1-telematica'
 
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
