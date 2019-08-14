36fa9de937749f12104a0fc49d484dc93bd6954d
danidehs <hsdaniele95@gmail.com>
2019-08-14 00:29:40 -0500

    prueba mongo docker

--- src/database.js
+++ src/database.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose')
 
-const URI = 'mongodb://mongo-server/proyecto1-telematica'
+// const URI = 'mongodb://mongo-server/proyecto1-telematica'
+
+var URI = {
+test: {
+        baseUrl: "/",
+        root: rootPath,
+        app: {
+          name: 'comentarios'
+        },
+        port: process.env.PORT || 3000,
+        db: 'mongodb://mongo-server/comentarios-test'
+      },
+}
 
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err))
 
-module.exports = mongoose
+//module.exports = mongoose
+module.exports = URI[env]
