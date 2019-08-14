856252b872201387896d5b88353ff32a7ad1b971
danidehs <hsdaniele95@gmail.com>
2019-08-14 00:38:24 -0500

    fix

--- src/database.js
+++ src/database.js
@@ -1,22 +1,9 @@
-const mongoose = require('mongoose')
+var mongoose = require('mongoose')
 
-// const URI = 'mongodb://mongo-server/proyecto1-telematica'
-
-var URI = {
-test: {
-        baseUrl: "/",
-        root: rootPath,
-        app: {
-          name: 'comentarios'
-        },
-        port: process.env.PORT || 3000,
-        db: 'mongodb://mongo-server/comentarios-test'
-      },
-}
+var URI = 'mongodb://localhost/proyecto1-telematica'
 
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err))
 
-//module.exports = mongoose
-module.exports = URI[env]
+module.exports = mongoose
